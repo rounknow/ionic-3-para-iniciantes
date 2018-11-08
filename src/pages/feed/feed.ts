f1472aa00ba7e25c7c6616b43a14c0ea3e1c4056
Ronaldo Moreira <rounkinfinity@hotmail.com>
2018-11-08 21:19:06 -0200

    Segundo commit

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,26 +13,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MoovieProvider
+  ]
 })
 export class FeedPage 
 {
+  public objetoFeed = 
+  {
+    titulo: "Charles Franca",
+    data: "November 5, 1995",
+    descricao: "Estou criando um app incrivel...",
+    quantLikes: 12,
+    quantComments: 4,
+    tempoComment: "11h ago teste"
+
+  }
+
+  public listaFilmes = new Array<any>();
+
+
   public nomeUsuario: string = "Charles Franca do Codigo";
 	
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private movieProvider: MoovieProvider) 
   {
 	  
   }
 	
   public somaDoisNumeros(num1:number, num2:number): void
   {
-	alert(num1 + num2);	
+	  alert(num1 + num2);	
   }
 
   ionViewDidLoad() 
   {
-    console.log('ionViewDidLoad FeedPage');
-	//this.somaDoisNumeros(5, 10);
+    this.movieProvider.getLatestMovies().subscribe(data=>
+    {
+      const response = (data as any);
+      const objetoRetorno = JSON.parse(response._body);
+      this.listaFilmes = objetoRetorno.results;
+      console.log(objetoRetorno);
+    },
+    error => 
+    {
+      console.log(error);
+    });
   }
 
 }
---
+++ src/providers/moovie/moovie.ts
@@ -0,0 +1,30 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+/*
+  Generated class for the MoovieProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MoovieProvider {
+
+  public baseAPIPath = "https://api.themoviedb.org/3";
+
+  constructor(public http: Http) 
+  {
+    console.log('Hello MoovieProvider Provider');
+  }
+
+  getLatestMovies()
+  {
+    return this.http.get(this.baseAPIPath + "/movie/popular?api_key=" + this.getAPIKey());
+  }
+
+  getAPIKey() : string
+  {
+    return "a626ef90306349eca33ef6048c2c9e1b";
+  }
+}
